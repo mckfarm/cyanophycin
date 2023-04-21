a32b460174d2cdec0a9e3bc8237efecab0ed8df7
unknown <mckennafarmer2023@u.northwestern.edu>
2023-04-21 13:23:27 -0500

    added plotting script

---
+++ plotting.R
@@ -0,0 +1,7 @@
+## ---------------------------
+## plotting params
+## ---------------------------
+
+theme_defaults <- list(
+  theme_classic(),
+  theme(legend.position="top", plot.title = element_text(face = "bold")))
